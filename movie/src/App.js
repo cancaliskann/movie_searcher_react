76d524add6265e3e6a6e15196f81ca9583d2f317
can caliskan <podcastwerv2017@gmail.com>
2024-03-21 19:19:55 +0200

    added search function enter key searches color scheme changed updated readme

--- movie/src/App.js
+++ movie/src/App.js
@@ -4,7 +4,7 @@ import { useEffect, useState } from "react";
 import './index.css';
 import SearchIcon from './search.svg'
 import MovieCard from "./MovieCard";
-const API_KEY = '9030ba7'
+const API_KEY = 'api key here'
 const API_URL = 'http://omdbapi.com?apikey=' + API_KEY;
 
 
@@ -12,7 +12,7 @@ const API_URL = 'http://omdbapi.com?apikey=' + API_KEY;
 
 function App() {
 const [movies, setMovies] = useState([]);
-
+const [searchTerm, setSearchTerm] = useState('');
   const searchMovies = async (title) => {
     console.log('Calling searchMovies function with title:', title);
     try {
@@ -23,7 +23,11 @@ const [movies, setMovies] = useState([]);
       console.error('Error fetching movies:', error);
     }
   }
-
+  const handleKeyPress = (e) => {
+    if (e.key === 'Enter') {
+      searchMovies(searchTerm);
+    }
+  };
 
   useEffect(() => {
     searchMovies('BatMan');
@@ -32,17 +36,18 @@ const [movies, setMovies] = useState([]);
 
   return (
     <div className="app">
-      <h1> MovieTest </h1>
+      <h1> Search Movies </h1>
       <div className="search">
         <input 
         placeholder="Search for movies"
-        value="Batman"
-        onChange={() => {}}
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value) }
+        onKeyPress={handleKeyPress}
         />
 
         <img src={SearchIcon} 
         alt="search"
-        onClick={() => {}}
+        onClick={() => searchMovies(searchTerm) }
         />
       </div>
 
